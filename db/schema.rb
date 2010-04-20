f2b614986b1cbe0d1c1119b03b117ada7258eb1e
Damien Desrousseaux <damien@awe.fr>
2010-04-20 11:31:57 +0200

    db migration: add email to user

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100420085721) do
+ActiveRecord::Schema.define(:version => 20100420091243) do
 
   create_table "auctions", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20100420085721) do
     t.string   "lastname"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
   create_table "wallets", :force => true do |t|
